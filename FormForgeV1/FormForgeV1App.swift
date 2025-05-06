5d4525ea6fa606b8a063ec0008d44c65fd144d16
KowalewskiPawel <pawelkowalewsk@gmail.com>
2025-05-06 09:53:31 +0200

    Initial Commit

---
+++ FormForgeV1/FormForgeV1App.swift
@@ -0,0 +1,17 @@
+//
+//  FormForgeV1App.swift
+//  FormForgeV1
+//
+//  Created by Pawel Kowalewski on 06/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct FormForgeV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
